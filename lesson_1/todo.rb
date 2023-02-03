9251bb3e79d4be21e587d071cb1441df5eb8e4b7
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-03 16:14:35 -0500

    Update each

--- lesson_1/todo.rb
+++ lesson_1/todo.rb
@@ -120,6 +120,7 @@ class TodoList
       yield(@todos[counter])
       counter += 1
     end
+    self
   end
 
   def select
@@ -138,18 +139,3 @@ class TodoList
     index < size
   end
 end
-
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
-
-list = TodoList.new("Today's Todos")
-list.add(todo1)
-list.add(todo2)
-list.add(todo3)
-
-todo1.done!
-
-results = list.select { |todo| todo.done? }    # you need to implement this method
-
-puts results.inspect
