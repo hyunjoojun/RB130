b1eb0abff23e9086a5322950604cb83e6594aea1
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-03 12:54:58 -0500

    Select Method

---
+++ lesson_1/select.rb
@@ -0,0 +1,18 @@
+def select(array)
+  counter = 0
+  new_arr = []
+
+  while counter < array.size
+    element = yield(array[counter])
+    new_arr << array[counter] if element
+    counter += 1
+  end
+
+  new_arr
+end
+
+array = [1, 2, 3, 4, 5]
+
+p select(array) { |num| num.odd? }      # => [1, 3, 5]
+p select(array) { |num| puts num }      # => [], because "puts num" returns nil and evaluates to false
+p select(array) { |num| num + 1 }       # => [1, 2, 3, 4, 5], because "num + 1" evaluates to true
