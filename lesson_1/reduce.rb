7e66819cae8173db487625afee221472429382bb
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-03 14:43:38 -0500

    Reduce

---
+++ lesson_1/reduce.rb
@@ -0,0 +1,25 @@
+def reduce(array, default=0)
+  counter = 0
+  acc = if array.first.class == Integer
+          default
+        elsif array.first.class == String
+          ''
+        else
+          []
+        end
+
+  while counter < array.size
+    acc = yield(acc, array[counter])
+    counter += 1
+  end
+
+  acc
+end
+
+array = [1, 2, 3, 4, 5]
+
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+# p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
