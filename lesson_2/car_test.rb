4b2739bf89af469a973438b27662344022c2ace1
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-06 15:41:55 -0500

    Car

--- lesson_2/car_test.rb
+++ lesson_2/car_test.rb
@@ -4,9 +4,38 @@ Minitest::Reporters.use!
 
 require_relative 'car'
 
-describe 'Car#wheels' do
-  it 'has 4 wheels' do
+class CarTest < Minitest::Test
+  def test_car_exists
     car = Car.new
-    _(car.wheels).must_equal 4
+    assert(car)
+  end
+
+  def test_wheels
+    car = Car.new
+    assert_equal(4, car.wheels)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")
+    end
+  end
+
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  def test_includes_car
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+
+    assert_includes(arr, car)
   end
 end
