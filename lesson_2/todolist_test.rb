390dfee8d5c5e73ac52424b62ca9959900dfe0d7
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-06 16:44:25 -0500

    Practice

--- lesson_2/todolist_test.rb
+++ lesson_2/todolist_test.rb
@@ -18,6 +18,92 @@ class TodoListTest < MiniTest::Test
     @list.add(@todo3)
   end
 
+  def test_to_a
+    assert_equal(@todos, @list.to_a)
+  end
 
+  def test_size
+    assert_equal(3, @list.size)
+  end
 
+  def test_first
+    assert_equal(@todo1, @list.first)
+  end
+
+  def test_last
+    assert_equal(@todo3, @list.last)
+  end
+
+  def test_shift
+    todo = @list.shift
+    assert_equal(@todo1, todo)
+    assert_equal([@todo2, @todo3], @list.to_a)
+  end
+
+  def test_pop
+    todo = @list.pop
+    assert_equal(@todo3, todo)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
+
+  def test_done_question
+    assert_equal(false, @list.done?)
+  end
+
+  def test_shovel
+    new_todo = Todo.new('Study')
+    @list << new_todo
+    @todos << new_todo
+
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_add
+    new_todo = Todo.new('Walk the dog')
+    @list.add(new_todo)
+    @todos << new_todo
+
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_item_at
+    assert_raises(IndexError) { @list.item_at(20) }
+    assert_equal(@todo1, @list.item_at(0))
+    assert_equal(@todo2, @list.item_at(1))
+  end
+
+  def test_mark_done_at
+    assert_raises(IndexError) { @list.mark_done_at(30) }
+    @list.mark_done_at(0)
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(false, @todo3.done?)
+  end
+
+  def test_mark_undone_at
+    assert_raises(IndexError) { @list.mark_undone_at(40) }
+    @todo1.done!
+    @todo2.done!
+    @todo3.done!
+
+    @list.mark_undone_at(1)
+
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_done_bang
+    @list.done!
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+    assert_equal(true, @list.done?)
+  end
+
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(50) }
+    @list.remove_at(2)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
 end
