b41d4e3b64dd8ca5464442a67b6cd49915dc5566
Hyunjoo Jun <hjj102088@gmail.com>
2023-02-27 16:18:17 -0500

    Reduce

--- lesson_1/reduce.rb
+++ lesson_1/reduce.rb
@@ -1,16 +1,11 @@
-def reduce(array, default=0)
-  counter = 0
-  acc = if array.first.class == Integer
-          default
-        elsif array.first.class == String
-          ''
-        elsif array.first.class == Array
-          []
-        end
+def reduce(collection, default=nil)
+  acc = default || collection[0]
+  index = default ? 0 : 1
 
-  while counter < array.size
-    acc = yield(acc, array[counter])
-    counter += 1
+  loop do
+    acc = yield(acc, collection[index])
+    index += 1
+    break if index >= collection.length
   end
 
   acc
@@ -18,8 +13,9 @@ end
 
 array = [1, 2, 3, 4, 5]
 
-p reduce(array) { |acc, num| acc + num }                    # => 15
-p reduce(array, 10) { |acc, num| acc + num }                # => 25
+p reduce(array) { |acc, num| acc + num } == 15
+p reduce(array, 10) { |acc, num| acc + num } == 25
 # p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
-p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
-p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value } == 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } == [1, 2, 'a', 'b']
+p reduce(array, 1) { |acc, num| acc + num } == 16
